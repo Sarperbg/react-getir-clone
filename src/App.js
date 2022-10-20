acb514a1456dbcb76067e8de27c2d91d33277b86
sarperbg <sarpergulfirat@gmail.com>
2022-10-20 16:20:34 +0300

    categories and campaigns completed

--- src/App.js
+++ src/App.js
@@ -10,15 +10,20 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-    <Header />
-    <HeroSection />
-    <Categories />
-    <Campaigns />
-    <Favorites />
-    <MobileApp />
-    <Cards />
-    <Footer />
-     
+      <Header />
+      <HeroSection />
+      <Categories />
+      <Campaigns />
+      <div className="container mx-auto">
+      <Favorites />
+      <MobileApp />
+      <Cards />
+      <Favorites />
+      <MobileApp />
+      <Cards />
+      </div>
+      <Footer />
+
     </>
   );
 }
