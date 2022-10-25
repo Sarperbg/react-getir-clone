2e42b7e59d097a706a770695cab0bc2a4a34088b
sarperbg <sarpergulfirat@gmail.com>
2022-10-25 17:03:05 +0300

    responsive - css bug fix completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import Header from "./components/Header";
-import HeroSection from "./components/HeroSection";
-import Categories from "./components/Categories";
-import Campaigns from "./components/Campaigns";
-import Favorites from "./components/Favorites";
-import MobileApp from "./components/MobileApp";
-import Cards from "./components/Cards";
-import Footer from "./components/Footer";
+import Header from 'components/Header'
+import HeroSection from 'components/HeroSection'
+import Categories from 'components/Categories'
+import Campaigns from 'components/Campaigns'
+import Favorites from 'components/Favorites'
+import MobileApp from 'components/MobileApp'
+import Cards from 'components/Cards'
+import Footer from 'components/Footer'
+import { useWindowWidth } from '@react-hook/window-size'
 
 function App() {
+  const windowWidth = useWindowWidth()
+
   return (
     <>
       <Header />
+      {windowWidth <= 768 && <Campaigns />}
       <HeroSection />
       <Categories />
-      <Campaigns />
-      <div className="container mx-auto grid gap-y-6">
-      <Favorites />
-      <MobileApp />
-      <Cards />
+      {windowWidth > 768 && <Campaigns />}
+      <div className="container mx-auto grid gap-y-6 pt-8">
+        <Favorites />
+        <MobileApp />
+        <Cards />
       </div>
       <Footer />
-
     </>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,15 +1,17 @@
-import { useState, useEffect } from "react"
-import cardsData from 'api/cards'
+import { useState, useEffect } from 'react';
+import cardsData from 'api/cards';
+
 export default function Cards() {
 
-    const [cards, setCards] = useState([]);
-    
+    const [cards , setCards] = useState([]);
+
     useEffect(() => {
-        //data request
+        // data isteği
         setCards(cardsData)
-    })
+    }, [])
+    
     return (
-        <div className="grid grid-cols-3 gap-x-4">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 px-4 md:px-0 gap-4">
             {cards.length && cards.map(card => (
                 <div className="bg-white p-14 rounded-lg shadow-sm flex flex-col items-center text-center">
                     <img className="w-[150px] h-[150px] mb-6" src={card.image} />
